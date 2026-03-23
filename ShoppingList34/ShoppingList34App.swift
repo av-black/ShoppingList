b2465dfc58e6a445766977538a142253d18a0d12
Nikita Tsomuk <tsomuk@me.com>
2026-03-23 17:18:45 +0200

    Initial Commit

---
+++ ShoppingList34/ShoppingList34App.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingList34App.swift
+//  ShoppingList34
+//
+//  Created by Nikita Tsomuk on 23.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingList34App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
