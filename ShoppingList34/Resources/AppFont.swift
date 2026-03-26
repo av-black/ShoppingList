6275de46631166614d571a2389b116935f302ab2
Alla Mozharova <alik19.09@mail.ru>
2026-03-26 21:45:18 +0300

    [Feature] Add text styles system (AppFont)

---
+++ ShoppingList34/Resources/AppFont.swift
@@ -0,0 +1,23 @@
+//
+//  AppFont.swift
+//  ShoppingList34
+//
+//  Created by Алла on 26.03.2026.
+//
+
+import SwiftUI
+
+enum AppFont {
+    static let largeTitle = Font.system(size: 34, weight: .regular)
+    static let title1 = Font.system(size: 28, weight: .semibold)
+    static let title2 = Font.system(size: 22, weight: .semibold)
+    static let title3 = Font.system(size: 20, weight: .medium)
+
+    static let headline = Font.system(size: 17, weight: .medium)
+    static let headlineStrong = Font.system(size: 17, weight: .semibold)
+    
+    static let sectionTitle = Font.system(size: 16, weight: .regular)
+    static let body = Font.system(size: 17, weight: .regular)
+    
+    static let caption = Font.system(size: 13, weight: .regular)
+}
