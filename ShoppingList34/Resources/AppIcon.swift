e32e6ecaeb547dfb9f04c9195c1598c4d0ac13b7
Alla Mozharova <alik19.09@mail.ru>
2026-03-26 19:59:02 +0300

    [Feature] Add AppIcon enum for managing icons

---
+++ ShoppingList34/Resources/AppIcon.swift
@@ -0,0 +1,26 @@
+//
+//  AppIcon.swift
+//  ShoppingList34
+//
+//  Created by Алла on 26.03.2026.
+//
+
+import SwiftUI
+
+enum AppIcon {
+    static let more = "ellipsis.circle"
+    static let back = "chevron.left"
+    static let clear = "xmark.circle.fill"
+    static let edit = "square.and.pencil"
+    static let duplicate = "plus.square.on.square"
+    static let trash = "trash"
+    static let theme = "circle.lefthalf.filled"
+    static let sort = "arrow.up.arrow.down"
+    static let chevronDown = "chevron.down"
+    static let checkmark = "checkmark"
+    static let selector = "chevron.up.chevron.down"
+    static let checkbox = "square"
+    static let checkboxSelected = "checkmark.square.fill"
+    static let share = "square.and.arrow.up"
+    static let reset = "arrow.trianglehead.2.clockwise.rotate.90"
+}
