1f76702336baf10996ecc3d5d36e4e99f087c25b
Anton Silenin <anton-silenin@yandex.ru>
2026-04-03 15:15:55 +0300

    [Feature] Add NoListsPlaceholderView and NoGoodsPlaceholderView

---
+++ ShoppingList34/Screens/Goods/Components/NoGoodsPlaceholderView.swift
@@ -0,0 +1,34 @@
+//
+//  NoGoodsPlaceholderView.swift
+//  ShoppingList34
+//
+//  Created by Anton Silenin on 03.04.2026.
+//
+
+import SwiftUI
+
+struct NoGoodsPlaceholderView: View {
+    var body: some View {
+        VStack(spacing: 16) {
+            AppIcon.itemsEmptyState.image
+                .resizable()
+                .scaledToFit()
+
+            VStack(spacing: 4) {
+                Text("Давайте спланируем покупки!")
+                    .font(AppFont.headline)
+                    .bold()
+
+                Text("Начните добавлять товары")
+                    .font(AppFont.body)
+                    .foregroundStyle(.secondary)
+            }
+        }
+        .multilineTextAlignment(.center)
+        .padding()
+    }
+}
+
+#Preview {
+    NoGoodsPlaceholderView()
+}
---
+++ ShoppingList34/Screens/List/Components/NoListsPlaceholderView.swift
@@ -0,0 +1,34 @@
+//
+//  NoListsPlaceholderView.swift
+//  ShoppingList34
+//
+//  Created by Anton Silenin on 03.04.2026.
+//
+
+import SwiftUI
+
+struct NoListsPlaceholderView: View {
+    var body: some View {
+        VStack(spacing: 16) {
+            AppIcon.mainEmptyState.image
+                .resizable()
+                .scaledToFit()
+
+            VStack(spacing: 4) {
+                Text("Давайте спланируем покупки!")
+                    .font(AppFont.headline)
+                    .bold()
+
+                Text("Создайте свой первый список")
+                    .font(AppFont.body)
+                    .foregroundStyle(.secondary)
+            }
+        }
+        .multilineTextAlignment(.center)
+        .padding()
+    }
+}
+
+#Preview {
+    NoListsPlaceholderView()
+}
