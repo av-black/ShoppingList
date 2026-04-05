3d8927c45803f6adfd0256b6ebf32ca1cf3480f9
Alla Mozharova <alik19.09@mail.ru>
2026-04-05 19:56:40 +0300

    [Refactor] Updated preview to use shared mock list and applied new fonts

---
+++ ShoppingList34/Screens/ShoppingItem/Components/ShoppingItemViewCell.swift
@@ -0,0 +1,86 @@
+//
+//  ShoppingItemViewCell.swift
+//  ShoppingList34
+//
+//  Created by Алла on 05.04.2026.
+//
+
+import SwiftUI
+
+struct ShoppingItemViewCell: View {
+    let shoppingItem: ShoppingItem
+    let onCheckboxTap: () -> Void
+    
+    var body: some View {
+        HStack(spacing: 8) {
+            Button(
+                action: onCheckboxTap,
+                label: {
+                    checkboxView
+                        .frame(width: 44, height: 44)
+                }
+            )
+            .buttonStyle(.plain)
+            
+            HStack {
+                Text(shoppingItem.title)
+                
+                Spacer()
+                
+                Text(amountText)
+            }
+            .font(AppFont.body)
+            .foregroundStyle(textColor)
+        }
+        .padding(.horizontal, 16)
+        .padding(.vertical, 4)
+    }
+    
+    // MARK: - Private Computed Properties
+    
+    private var amountText: String {
+        "\(shoppingItem.formattedAmount) \(shoppingItem.type.rawValue)."
+    }
+    
+    private var checkboxView: some View {
+        Group {
+            if shoppingItem.isComplete {
+                ZStack {
+                    AppIcon.checkboxFill.image
+                        .font(AppFont.checkbox)
+                        .foregroundColor(.turquoiseUniversalSL)
+                    
+                    AppIcon.checkmark.image
+                        .font(AppFont.checkboxMark)
+                        .foregroundColor(.whiteUniversalSL)
+                }
+            } else {
+                AppIcon.checkbox.image
+                    .font(AppFont.checkbox)
+                    .foregroundColor(.grayCheckboxSL)
+            }
+        }
+        
+    }
+    
+    private var textColor: Color {
+        shoppingItem.isComplete ? .graySelectedTextSL : .blackTextSL
+    }
+}
+
+#Preview {
+    ScrollView {
+        VStack(spacing: 0) {
+            ForEach(ListItem.mock.shoppingItem) { item in
+                ShoppingItemViewCell(
+                    shoppingItem: item,
+                    onCheckboxTap: {}
+                )
+
+                Divider()
+                    .background(Color.graySeparatorSL)
+            }
+        }
+        .background(.grayMainBackgroundSL)
+    }
+}
