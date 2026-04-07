4a5f42d5034754ab58ceac94ff60a8c4c2e50dff
Anton Silenin <anton-silenin@yandex.ru>
2026-04-07 20:23:42 +0300

    [refactor] use private constant to store deafultUser key name

--- ShoppingList34/App/AppState.swift
+++ ShoppingList34/App/AppState.swift
@@ -10,12 +10,16 @@ import SwiftUI
 @Observable
 final class AppState {
     var hasLaunchedBefore: Bool {
-        get { UserDefaults.standard.bool(forKey: "hasLaunchedBefore") }
-        set { UserDefaults.standard.set(newValue, forKey: "hasLaunchedBefore") }
+        get { UserDefaults.standard.bool(forKey: Keys.hasLaunchedBefore) }
+        set { UserDefaults.standard.set(newValue, forKey: Keys.hasLaunchedBefore) }
     }
 
     /// Вызывается при нажатии "Начать" на WelcomeView
     func completeOnboarding() {
         hasLaunchedBefore = true
     }
+}
+
+private enum Keys {
+    static let hasLaunchedBefore = "hasLaunchedBefore"
 }
