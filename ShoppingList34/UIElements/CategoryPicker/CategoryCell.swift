fb902ae4b9db4a990328195d77620da4bb775ba5
RacyFiend <demonperechemidi@gmail.com>
2026-04-08 22:10:43 +0300

    [Feature] Add CategoryPickerView with single selection and customizable highlight color

---
+++ ShoppingList34/UIElements/CategoryPicker/CategoryCell.swift
@@ -0,0 +1,39 @@
+//
+//  CategoryCell.swift
+//  ShoppingList34
+//
+//  Created by Дмитрий Перчемиди on 07.04.2026.
+//
+
+import SwiftUI
+
+struct CategoryCell: View {
+    
+    let item: CategoryItem
+    let isSelected: Bool
+    let selectedColor: Color
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(
+                    isSelected
+                    ? selectedColor
+                    : .grayIconBackgroundSL
+                )
+                .frame(maxWidth: .infinity)
+                .aspectRatio(1, contentMode: .fit)
+            
+            item.icon.image
+                .foregroundStyle(
+                    isSelected
+                    ? .blackUniversalSL
+                    : .whiteUnselectedIconSL
+                )
+                .scaledToFit()
+        }
+        .frame(maxWidth: .infinity)
+        .aspectRatio(1, contentMode: .fit)
+        
+    }
+}
---
+++ ShoppingList34/UIElements/CategoryPicker/CategoryPickerView.swift
@@ -0,0 +1,75 @@
+//
+//  CategoryPickerView.swift
+//  ShoppingList34
+//
+//  Created by Дмитрий Перчемиди on 07.04.2026.
+//
+
+import SwiftUI
+
+struct CategoryPickerView: View {
+    
+    let items: [CategoryItem]
+    @Binding var selectedItem: CategoryItem
+    let selectedColor: Color
+    
+    private let columns = Array(
+        repeating: GridItem(.flexible(), spacing: 12),
+        count: 6
+    )
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            
+            Text("Выберите дизайн")
+                .font(AppFont.sectionTitle)
+                .padding(.vertical, 12)
+                .padding(.horizontal, 16)
+                
+            LazyVGrid(
+                columns: columns,
+                spacing: 12
+            ) {
+                ForEach(items) { item in
+                    CategoryCell(
+                        item: item,
+                        isSelected: item.id == selectedItem.id,
+                        selectedColor: selectedColor
+                    )
+                    .onTapGesture {
+                        handleSelect(item)
+                    }
+                }
+            }
+            .padding(.bottom, 16)
+            .padding(.horizontal, 20)
+            
+        }
+        .background(.grayCardBackgroundSL)
+        .clipShape(RoundedRectangle(cornerRadius: 16))
+    }
+    
+    private func handleSelect(_ item: CategoryItem) {
+        selectedItem = item
+    }
+}
+
+
+struct PreviewWrapper: View {
+    
+    @State private var selected = CategoryItem(icon: AppIcon.airplane)
+    
+    var body: some View {
+        CategoryPickerView(
+            items: CategoryItem.mockCategoryItems,
+            selectedItem: $selected,
+            selectedColor: ListColor.blue.color,
+        )
+    }
+}
+
+#Preview {
+    PreviewWrapper()
+        .padding()
+        .background(Color(.systemGroupedBackground))
+}
