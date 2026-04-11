c51d8f83fda355e4ca89142e0f566cd55c8f6116
Alla Mozharova <alik19.09@mail.ru>
2026-04-11 17:38:35 +0300

    [Refactor] Make ModalRoute Identifiable for sheet navigation

--- ShoppingList34/App/Navigation/ModalRoute.swift
+++ ShoppingList34/App/Navigation/ModalRoute.swift
@@ -8,7 +8,16 @@
 import Foundation
 
 /// Маршруты модальной навигации приложения.
-enum ModalRoute: Hashable {
+enum ModalRoute: Hashable, Identifiable {
     case createShoppingItem
     case editShoppingItem(item: ShoppingItem)
+    
+    var id: String {
+        switch self {
+        case .createShoppingItem:
+            return "createShoppingItem"
+        case .editShoppingItem(let item):
+            return "editShoppingItem_\(item.id)"
+        }
+    }
 }
